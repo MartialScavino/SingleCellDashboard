7fdb269181a9f55e3ea6858a53983719d5fdb37b
MartialScavino <106530336+MartialScavino@users.noreply.github.com>
2023-06-06 14:59:55 +0200

    Nom de genes sur volcano plot

--- R/AllMarkers.R
+++ R/AllMarkers.R
@@ -184,7 +184,7 @@ allmarkersserver <- function(input, output, session, val){
     top10down <- subdf_down %>% slice_max(n = 10, order_by = -avg_log2FC)
     
     ggplotly(
-      ggplot(subdf, aes(x = avg_log2FC, y = -log10(p_val_adj))) +
+      ggplot(subdf, aes(x = avg_log2FC, y = -log10(p_val_adj), label = gene)) +
         geom_point(data = subdf_up, color = "red") + 
         geom_point(data = subdf_down, color = "blue") + 
         geom_text(data = top10up, aes(x = avg_log2FC, y = -log10(p_val_adj), label = gene), nudge_y = 2) +
